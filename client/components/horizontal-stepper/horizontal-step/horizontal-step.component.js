2ed0955eedf12be5ca6c184638fcc2a78d84e156
Nick Ocampo <nick@wehatenick.com>
2017-06-27 12:53:09 -0700

    styled body

--- client/components/horizontal-stepper/horizontal-step/horizontal-step.component.js
+++ client/components/horizontal-stepper/horizontal-step/horizontal-step.component.js
@@ -1,22 +1,22 @@
 import angular from 'angular';
 
 export class HorizontalStepComponent {
-	$onInit() {
-		this.lastStep = this.horizontalStepperCtrl.steps[this.horizontalStepperCtrl.steps.length - 1];
-	}
-	// Get the active step
-	$doCheck() {
-	  if (this.horizontalStepperCtrl.activeStep === this.label) {
-	  	this.active = true;
-	  } else {
-	  	this.active = false;
-	  }
-	  if (this.horizontalStepperCtrl.completedSteps.includes(this.label)) {
-	  	this.complete = true;
-	  } else {
-	  	this.complete = false;
-	  }
-	}
+	// $onInit() {
+	// 	this.lastStep = this.horizontalStepperCtrl.steps[this.horizontalStepperCtrl.steps.length - 1];
+	// }
+	// // Get the active step
+	// $doCheck() {
+	//   if (this.horizontalStepperCtrl.activeStep === this.label) {
+	//   	this.active = true;
+	//   } else {
+	//   	this.active = false;
+	//   }
+	//   if (this.horizontalStepperCtrl.completedSteps.includes(this.label)) {
+	//   	this.complete = true;
+	//   } else {
+	//   	this.complete = false;
+	//   }
+	// }
 }
 
 export default angular.module('directives.horizontalStep', [])
