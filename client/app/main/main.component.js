253c1b6aa863b8899d81a403bc4c2f32c026d7a8
Nick Ocampo <nick@wehatenick.com>
2017-07-01 00:10:30 -0700

    finished shadow dom business

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -24,50 +24,42 @@ export class MainController {
           number: 1,
           content: 'We have completed an inventory of our software portfolio (including in-house development, COTS, third-party development, etc.)',
           dbEntity: 'hpeQuestion1',
-          analyticsName: 'Question 1',
-          responded: false
+          analyticsName: 'Question 1'
         }, {
           number: 2,
           content: 'Applications in our portoflio have been classified according to data type',
           dbEntity: 'hpeQuestion2',
-          analyticsName: 'Question 2',
-          responded: false
+          analyticsName: 'Question 2'
         }, {
           number: 3,
           content: 'We have completed a risk exposure anlaysis of our business critical applications',
           dbEntity: 'hpeQuestion3',
-          analyticsName: 'Question 3',
-          responded: false
+          analyticsName: 'Question 3'
         }, {
           number: 4,
           content: 'Our security goals are documented and communciated to stakeholders',
           dbEntity: 'hpeQuestion4',
-          analyticsName: 'Question 4',
-          responded: false
+          analyticsName: 'Question 4'
         }, {
           number: 5,
           content: 'There is a specific group/team that is responsible for Appsec',
           dbEntity: 'hpeQuestion5',
-          analyticsName: 'Question 5',
-          responded: false
+          analyticsName: 'Question 5'
         }, {
           number: 6,
           content: 'We have documented and communicated secure coding standards to everyone involved in software development',
           dbEntity: 'hpeQuestion6',
-          analyticsName: 'Question 6',
-          responded: false
+          analyticsName: 'Question 6'
         }, {
           number: 7,
           content: 'Our software suppliers and development partners are required to find and fix security vulnerabilities',
           dbEntity: 'hpeQuestion7',
-          analyticsName: 'Question 7',
-          responded: false
+          analyticsName: 'Question 7'
         }, {
           number: 8,
           content: 'We have a role-based security education program in place  to support application security awareness',
           dbEntity: 'hpeQuestion8',
-          analyticsName: 'Question 8',
-          responded: false
+          analyticsName: 'Question 8'
         }]
       }, {
         label: 'B',
@@ -79,30 +71,26 @@ export class MainController {
           number: 9,
           content: 'Secure frameworks and design features are considered during the design process',
           dbEntity: 'hpeQuestion9',
-          analyticsName: 'Question 9',
-          responded: false
+          analyticsName: 'Question 9'
         }, {
           number: 10,
           content: 'We have a documented process track use of open source components and verify their security',
           dbEntity: 'hpeQuestion10',
-          analyticsName: 'Question 10',
-          responded: false
+          analyticsName: 'Question 10'
         }, {
           number: 11,
           content: 'Development teams follow secure coding practices',
           dbEntity: 'hpeQuestion11',
-          analyticsName: 'Question 11',
-          responded: false
+          analyticsName: 'Question 11'
         }, {
           number: 12,
           content: 'Threat models are built and maintained on a per-application basis',
           dbEntity: 'hpeQuestion12',
-          analyticsName: 'Question 12',
-          responded: false
+          analyticsName: 'Question 12'
         }],
       }, {
         label: 'C',
-        name: 'Verification',
+        name: 'Verification and Deployment',
         complete: false,
         active: false,
         description: 'Nulla eleifend, nunc eget mattis tristique, libero nibh porttitor lectus, a semper nisl nisi vitae sem. Morbi convallis congue lobortis. Vestibulum id tempus velit. Maecenas at semper dolor, non sollicitudin tortor.',
@@ -110,44 +98,37 @@ export class MainController {
           number: 13,
           content: 'We conduct regular secure code reviews',
           dbEntity: 'hpeQuestion13',
-          analyticsName: 'Question 13',
-          responded: false
+          analyticsName: 'Question 13'
         }, {
           number: 14,
           content: 'We conduct secure design reviews for new projects',
           dbEntity: 'hpeQuestion14',
-          analyticsName: 'Question 14',
-          responded: false
+          analyticsName: 'Question 14'
         }, {
           number: 15,
           content: 'Software architects are involved in the secure design reviews',
           dbEntity: 'hpeQuestion15',
-          analyticsName: 'Question 15',
-          responded: false
+          analyticsName: 'Question 15'
         }, {
           number: 16,
           content: 'We have an established security checkpoint and stakeholders are involved in go/no go decision before application goes into production',
           dbEntity: 'hpeQuestion16',
-          analyticsName: 'Question 16',
-          responded: false
+          analyticsName: 'Question 16'
         }, {
           number: 17,
           content: 'Stakeholders regularly review the results or metrics from the security code review activities',
           dbEntity: 'hpeQuestion17',
-          analyticsName: 'Question 17',
-          responded: false
+          analyticsName: 'Question 17'
         }, {
           number: 18,
           content: 'Development teams regularly  perform source code analysis as part of their workflow',
           dbEntity: 'hpeQuestion18',
-          analyticsName: 'Question 18',
-          responded: false
+          analyticsName: 'Question 18'
         }, {
           number: 19,
           content: 'We perform penetration tests on software releases',
           dbEntity: 'hpeQuestion19',
-          analyticsName: 'Question 19',
-          responded: false
+          analyticsName: 'Question 19'
         }]
       }, {
         label: 'D',
@@ -159,20 +140,17 @@ export class MainController {
           number: 20,
           content: 'In addition to our app security testing program, we wrap additional protection around  high-risk applications',
           dbEntity: 'hpeQuestion20',
-          analyticsName: 'Question 20',
-          responded: false
+          analyticsName: 'Question 20'
         }, {
           number: 21,
           content: 'We monitor applications running in production to collect and respond to threat intelligence',
           dbEntity: 'hpeQuestion21',
-          analyticsName: 'Question 21',
-          responded: false
+          analyticsName: 'Question 21'
         }, {
           number: 22,
           content: 'We have a complete feedback loop to share incidents and identified vulnerabilitiy information back to development and design teams',
           dbEntity: 'hpeQuestion22',
-          analyticsName: 'Question 22',
-          responded: false
+          analyticsName: 'Question 22'
         }]
       }
     ];
@@ -207,7 +185,7 @@ export class MainController {
     this.isSectionIncomplete = (questions) => {
       var isIncomplete = false;
       questions.forEach((question) => {
-        if (question.responded === false) {
+        if (!question.value) {
           isIncomplete = true;
         }
       });
